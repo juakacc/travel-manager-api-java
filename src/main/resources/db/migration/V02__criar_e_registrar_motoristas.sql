ea6c26ee9c3cb328edff370f32792d3952959107
Juaka <juakacc@gmail.com>
2020-02-10 11:32:01 -0300

    Ajustes para aws

--- src/main/resources/db/migration/V02__criar_e_registrar_motoristas.sql
+++ src/main/resources/db/migration/V02__criar_e_registrar_motoristas.sql
@@ -6,4 +6,6 @@ CREATE TABLE motorista (
 	categoria VARCHAR(10) NOT NULL , 
 	telefone VARCHAR(255) NOT NULL , 
 	senha VARCHAR(255) NOT NULL 
-) ENGINE = InnoDB CHARSET=utf8;
+) ENGINE = InnoDB DEFAULT CHARSET=utf8;
+
+INSERT INTO motorista (nome, apelido, cnh, categoria, telefone, senha) VALUES ('admin', 'admin', '', 'AB', '', '$2a$10$4.EQP.0yiZBQtoy4Qf77tuHEJqiP3jovuJLhjw57.wWymrz6TzrSG');
